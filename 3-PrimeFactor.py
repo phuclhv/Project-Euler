dee5039017881809f1a5b4f81a472dd96dac3315
Phuc-HomeUbuntu <lephuc66@gmail.com>
2019-10-07 14:54:26 -0700

    Optimize solution for problem 3

--- 3-PrimeFactor.py
+++ 3-PrimeFactor.py
@@ -4,27 +4,31 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 '''
+
 import math
 def primeFactor(value):
   primeArr = [2]
+  while (value % 2 == 0):
+    value = value / 2
+    lastFactor = 2
+
   def checkPrime(num):
-    for i in range(len(primeArr)):
-      if num % primeArr[i] == 0:
+    for pastPrime in primeArr:
+      if (num % pastPrime == 0):
         return False
-      if primeArr[i] >= math.sqrt(num):
-        return True
     return True
 
-  currNum = 3
-  res = value
-  while value != 1:
-    if checkPrime(currNum):
-      primeArr.append(currNum)
-      while (value % currNum == 0):
-        value = value // currNum
-        res = currNum
-    currNum += 2
-  return res
+  limitFactor = math.sqrt(value)
+  factor = 3
+  
+  while value > 1 and factor < limitFactor:
+    if checkPrime(factor):
+      primeArr.append(factor)
+      while (value % factor == 0):
+        value = value // factor
+        lastFactor = factor
+    factor += 2
+  return lastFactor
 
 print(primeFactor(600851475143))
       
