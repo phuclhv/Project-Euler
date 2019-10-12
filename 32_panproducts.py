8aab5c3fa3fd33c8edf0d032a52bd236f5d01e71
Phuc-HomeUbuntu <lephuc66@gmail.com>
2019-10-12 10:04:54 -0700

    First working solution for prob 32. Brute force. Comment added

---
+++ 32_panproducts.py
@@ -0,0 +1,40 @@
+'''
+https://www.projecteuler.net/problem=32
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+'''
+'''
+1st Approach: Brute force with Math
+Assume that a < b and a*b = product is pandigital
+We have 2 * 5000 = 10000 > 9 total digits and 100 * 100 = 10000 > 9 total digits
+Thus a and b would be limit in range (2,100) and (100,5000) respectively with a < b
+'''
+from test import average_time_taken
+def sum_pan_product():
+
+  def is_pan(a,b):
+    product = a * b
+    product_str_sorted = ''.join(sorted(str(a) + str(b) + str(product)))
+    if product_str_sorted == "123456789":
+      return True
+    return  False
+  
+  res = 0
+  pan_set = set()
+  for i in range(2,1000):
+    for j in range(100,5000):
+      if j < i:
+        continue
+      if is_pan(i,j) and i*j not in pan_set:
+        pan_set.add(i*j)
+        res += (i*j)
+  
+  return res
+
+average_time_taken(sum_pan_product)
+#Average time taken is 9.35s. Result: 45228
