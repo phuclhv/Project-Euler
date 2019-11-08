f804fe23bb19d8ed00c0d90880bbdaacb550b867
Phuc-HomeUbuntu <lephuc66@gmail.com>
2019-11-07 18:35:56 -0800

    Change variable names

--- 9_pythago_triplet.py
+++ 9_pythago_triplet.py
@@ -1,7 +1,7 @@
 import math
-def productTriplet(sumABC):
+def product_triplet(sum):
   a = 1
-  while a < sumABC/3:
+  while a < sum/3:
     b = a + 1
     c = 1000 - a - b 
     dif = c*c - a*a - b*b
@@ -14,4 +14,4 @@ def productTriplet(sumABC):
     a+=1
   return 0
 
-print(productTriplet(1000))
+print(product_triplet(1000))
